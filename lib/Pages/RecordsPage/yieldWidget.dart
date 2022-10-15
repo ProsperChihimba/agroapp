a71192d803d9b7c80cf2b13f59393a870b40d7d0
ProsperChihimba <prosperchihimba@gmail.com>
2022-10-16 01:52:34 +0300

    Add: reports initial design

--- lib/Pages/RecordsPage/yieldWidget.dart
+++ lib/Pages/RecordsPage/yieldWidget.dart
@@ -2,74 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class YieldPrediction extends StatelessWidget {
+class YieldPrediction extends StatefulWidget {
   const YieldPrediction({super.key});
 
   @override
+  State<YieldPrediction> createState() => _YieldPredictionState();
+}
+
+class _YieldPredictionState extends State<YieldPrediction> {
+  List crops = ["All", "Maize", "Rice", "Potato", "Banana"];
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
-          margin: const EdgeInsets.only(top: 20),
-          decoration: BoxDecoration(
-              color: const Color(0xff000000),
-              borderRadius: BorderRadius.circular(23),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.withOpacity(0.6),
-                    offset: const Offset(0, 6),
-                    blurRadius: 5.0,
-                    spreadRadius: 0)
-              ]),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Maindi",
-                    style: GoogleFonts.lato(
-                        fontWeight: FontWeight.w900,
-                        fontSize: 16,
-                        color: Colors.white),
-                  ),
-                  Text(
-                    "20,000 tons",
-                    style: GoogleFonts.lato(
-                        fontWeight: FontWeight.w900,
-                        fontSize: 16,
-                        color: Colors.white),
-                  ),
-                ],
-              ),
-              const Gap(35),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "Tsh 200,000/=",
-                  style: GoogleFonts.lato(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 16,
-                      color: Colors.white),
-                ),
-              ),
-              const Gap(35),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "3 Spt 2022",
-                    style: GoogleFonts.lato(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14,
-                        color: Colors.white),
-                  ),
-                ],
-              ),
-            ],
-          )),
+      padding: const EdgeInsets.only(left: 25.0, top: 20.0),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.20,
+        child: ListView(
+            // This next line does the trick.
+            scrollDirection: Axis.horizontal,
+            children: crops.map(
+              (data) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: data == "All"
+                            ? const Color(0xffd2ddf7)
+                            : const Color(0xffdbd9d9),
+                      ),
+                      padding:
+                          const EdgeInsets.only(left: 80, right: 80, top: 50),
+                      // margin: const EdgeInsets.all(0.5),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          children: [
+                            Text(
+                              data,
+                              style: GoogleFonts.lato(
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 16,
+                                  color: Colors.black),
+                            ),
+                            Gap(10),
+                            Text(
+                              "Ton: 200",
+                              style: GoogleFonts.lato(
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 13,
+                                  color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      )),
+                );
+              },
+            ).toList()),
+      ),
     );
   }
 }
