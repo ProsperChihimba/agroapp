5ce2a6666ddb6bcb08de9323c351c323f4be2045
ProsperChihimba <prosperchihimba@gmail.com>
2022-10-18 11:10:34 +0300

    Update: add input fields in add record page

--- lib/Pages/AddRecord/index.dart
+++ lib/Pages/AddRecord/index.dart
@@ -17,9 +17,9 @@ class _AddRecordState extends State<AddRecord> {
         backgroundColor: Colors.white,
         elevation: 0,
         title: Text(
-          'New Record',
+          'New record',
           style: GoogleFonts.lato(
-              fontSize: 20,
+              fontSize: 16,
               fontWeight: FontWeight.w700,
               color: const Color(0xff141517)),
         ),
@@ -30,6 +30,90 @@ class _AddRecordState extends State<AddRecord> {
           },
           icon: const Icon(Icons.arrow_back_rounded),
           color: const Color(0xff000000),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(25.0, 50.0, 25.0, 0),
+            child: Form(
+              child: Column(
+                children: [
+                  Container(
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Tafadhali jaza kiasi ulichopokea";
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Kiasi ulichotumia",
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        labelStyle: GoogleFonts.lato(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12,
+                          color: Color(0xff143AFF),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xff9A9A9A))),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xff9A9A9A))),
+                        hintText: 'Jaza kiasi ulichotumia',
+                        hintStyle: GoogleFonts.lato(
+                            fontSize: 12, fontWeight: FontWeight.w400),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xff9A9A9A))),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xff9A9A9A))),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    height: 55,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Jaza Taarifa ya ziada hapa (sio lazima)',
+                        hintStyle: GoogleFonts.lato(
+                            fontSize: 12, fontWeight: FontWeight.w400),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xff9A9A9A))),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xff9A9A9A))),
+                      ),
+                    ),
+                  ),
+
+                  // Button
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  Container(
+                    height: 50,
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Color(0xff1D976C)),
+                        ),
+                        child: Center(
+                            child: Text(
+                          'Insert Record',
+                          style: GoogleFonts.lato(
+                              fontWeight: FontWeight.w700, fontSize: 16),
+                        ))),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
--- lib/Pages/MainPage/ReportButtons.dart
+++ lib/Pages/MainPage/ReportButtons.dart
@@ -1,3 +1,4 @@
+import 'package:agroapp/Pages/AddRecord/index.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,34 +11,42 @@ class ReportButtons extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Expanded(
-          child: Container(
-            padding: const EdgeInsets.all(12),
-            decoration: BoxDecoration(
-                color: const Color(0xff1D976C),
-                borderRadius: BorderRadius.circular(10)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 5.0, 0),
-                      child: Icon(
-                        Icons.add_circle,
-                        size: 25,
-                        color: Colors.white,
+          child: InkWell(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (BuildContext context) {
+                return const AddRecord();
+              }));
+            },
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                  color: const Color(0xff1D976C),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, 5.0, 0),
+                        child: Icon(
+                          Icons.add_circle,
+                          size: 25,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    Text(
-                      'Add Record',
-                      style: GoogleFonts.lato(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 15,
-                          color: Colors.white),
-                    )
-                  ],
-                ),
-              ],
+                      Text(
+                        'Add Record',
+                        style: GoogleFonts.lato(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 15,
+                            color: Colors.white),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
